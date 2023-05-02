c28ba6fd755d014802684689061b722984e3d1dd
KateePankratevaa <124819002+KateePankratevaa@users.noreply.github.com>
2023-05-02 13:33:51 +0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿
+namespace PRACTICA14_3_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Label label1;
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SortYiungButton = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button3 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            label1 = new System.Windows.Forms.Label();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(74, 58);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(58, 13);
+            label1.TabIndex = 0;
+            label1.Text = "Введите n";
+            label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.textBox1.Location = new System.Drawing.Point(77, 97);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(66, 143);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 40);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Загрузить";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(257, 5);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(260, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(263, 87);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(266, 142);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(263, 186);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "label6";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.numericUpDown1);
+            this.panel2.Controls.Add(this.SortYiungButton);
+            this.panel2.Controls.Add(this.dataGridView1);
+            this.panel2.Location = new System.Drawing.Point(221, 217);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(508, 221);
+            this.panel2.TabIndex = 9;
+            this.panel2.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(170, 17);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(258, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Оссортировать по возрасту";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // SortYiungButton
+            // 
+            this.SortYiungButton.Location = new System.Drawing.Point(19, 17);
+            this.SortYiungButton.Name = "SortYiungButton";
+            this.SortYiungButton.Size = new System.Drawing.Size(145, 23);
+            this.SortYiungButton.TabIndex = 1;
+            this.SortYiungButton.Text = "Оссортировать\r\n";
+            this.SortYiungButton.UseVisualStyleBackColor = true;
+            this.SortYiungButton.Click += new System.EventHandler(this.SortYiungButton_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 72);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(505, 126);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(44, 255);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(123, 49);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Показать задание 4";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(19, 46);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button SortYiungButton;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace PRACTICA14_3_5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+               
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n=0;
+           
+            if(textBox1.Text ==" ")
+            {
+                MessageBox.Show("Пустое поле");
+                n = Convert.ToInt32(textBox1.Text);
+            }
+            Queue Q = new Queue();
+            n = Convert.ToInt32(textBox1.Text);
+            for (int i = 1; i < n; i++)
+            {
+
+                Q.Enqueue(i);
+                label2.Text = ("Размер очереди равно " + Q.Count);
+                label3.Text = ("Верхний элемент в очереди" + Q.Peek());
+                label4.Text = ("Содержимое равно ");
+                while (Q.Count != 0)
+                {
+                    label5.Text ="{0}"+Q.Dequeue();
+                }
+                label6.Text = ("Новая размерность " + Q.Count);
+            }
+        }
+
+        Queue<string[]> peopleQueue;
+
+
+        private void Display()
+        {
+            DataTable datatable = new DataTable();
+            datatable.Columns.Add("Фамилия");
+            datatable.Columns.Add("Имя");
+            datatable.Columns.Add("Отчество");
+            datatable.Columns.Add("Возраст");
+            datatable.Columns.Add("Вес");
+            foreach (var person in peopleQueue)
+            {
+                datatable.Rows.Add(person[0], person[1], person[2], person[3], person[4]);
+            }
+            dataGridView1.DataSource = datatable;
+        }
+
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            panel2.Visible = true;
+            peopleQueue = new Queue<string[]>();
+            using (var sr = new StreamReader("text.txt"))
+            {
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine();
+                    if (line.Length == 0)
+                    {
+                        MessageBox.Show("Файл пуст");
+                    }
+                    else
+                    {
+                        var parts = line.Split(' ');
+                        peopleQueue.Enqueue(parts);
+                    }
+                }
+                Display();
+            }
+        }
+
+        private void SortYiungButton_Click(object sender, EventArgs e)
+        {
+            var youngPerson = new Queue<string[]>();
+            var oldPerson = new Queue<string[]>();
+            foreach (var person in peopleQueue)
+            {
+                var age = int.Parse(person[3]);
+                if (age < numericUpDown1.Value)
+                {
+                    youngPerson.Enqueue(person);
+                }
+                else
+                {
+                    oldPerson.Enqueue(person);
+                }
+                peopleQueue = new Queue<string[]>(youngPerson.Concat(oldPerson));
+
+            }
+            Display();
+        }
+
+      
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var list = peopleQueue.ToList();
+            list.Sort((a, b) => int.Parse(a[3]).CompareTo(int.Parse(b[3])));
+            peopleQueue = new Queue<string[]>(list);
+            Display();
+        }
+    }
+}
